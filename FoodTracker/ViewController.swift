9ca7adeba139b5749301b0e7c07df09e9f739908
Justin Han <jdhan@cpp.edu>
2020-06-30 20:31:03 -0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Justin Han on 6/30/20.
+//  Copyright © 2020 AsianBoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
